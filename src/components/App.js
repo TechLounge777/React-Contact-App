1e0971bd3bf3e0f37f038f9c97855561abfd4b91
TechLounge777 <msblanche777@gmail.com>
2021-06-28 19:23:29 +0200

    added addContact component

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
-//import AddContact from './AddContact';
-//import  ContactList from './ContactList';
+import AddContact from './AddContact';
+import ContactList from './ContactList';
 
-function App () {
+function App() {
+  const contacts = [
+    {
+      id: "1",
+      name: "John",
+      email: "John@gmail.com"
+    },
+    {
+      id: "2",
+      name: "Tom",
+      email: "Tom@gmail.com"
+    }
+  ];
   return (
-    <div className="App">
+    <div className="ui container">
       <Header />
-      {/*<AddContact />
-      <ContactList />*/}
+      <AddContact />
+      <ContactList />
     </div>
   );
 }
