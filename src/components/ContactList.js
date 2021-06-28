8648b88b197c08f4d4fbdb973f731580486b53d8
TechLounge777 <msblanche777@gmail.com>
2021-06-28 20:59:01 +0200

    added the ContactList component

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,11 +1,24 @@
 import React from 'react'
 
-function ContactList() {
+function ContactList(props) {
+
+    const renderContactList = props.contacts.map((contact) =>{
+        return (
+            <div className="item">
+                <div className="Content">
+                    <div className="header">{contact.name}</div>
+                    <div>{contact.email}</div>
+                    <i className="trash alternate outine icon"></i>
+                </div>
+            </div>
+        );
+    });
+
     return (
         <div className="ui celled list">
-            Contact List
+            {renderContactList}
         </div>
     )
-}
+};
 
 export default ContactList
