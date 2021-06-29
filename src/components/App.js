efe90b12f3442784035fbb30e49184711a9b25ba
TechLounge777 <msblanche777@gmail.com>
2021-06-29 13:46:31 +0200

    added useState

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Header from './Header';
 import AddContact from './AddContact';
 import ContactList from './ContactList';
 
 function App() {
-  const contacts = [
-    {
-      id: "1",
-      name: "John",
-      email: "John@gmail.com"
-    },
-    {
-      id: "2",
-      name: "Tom",
-      email: "Tom@gmail.com"
-    }
-  ];
+  const [contacts, setContacts] = useState([]);
   return (
     <div className="ui container">
       <Header />
